9d723f849507c002d1f1217442f0df5677569730
1BRG <balaceanurafael@gmail.com>
2025-11-10 16:12:16 +0200

    update MultinomialNaiveBayes class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
 
 from pandas.io.formats.format import return_docstring
 
@@ -10,8 +11,9 @@ csv_path = "Music-Databases/Light_Music_Database.csv"
 class MultinomialNaiveBayes:
     def __init__(self):
         self.a_priori = {}
-        self.vocabulary = {}
+        self.vocabulary = set()
         self.conditionals = {}
+        self.alpha = 1.0
 
     def calc_a_priori(self, y_train):
         genres = {}
@@ -28,13 +30,69 @@ class MultinomialNaiveBayes:
         for i in range (0, len(x_train)):
             l = x_train[i]
             genre = y_train[i]
-            for str in l:
+            if(genre not in self.conditionals):
+                self.conditionals[genre] = {}
+            for word in l:
+                self.vocabulary.add(word)
+                if word not in self.conditionals[genre]:
+                    self.conditionals[genre][word] = 1
+                else:
+                    self.conditionals[genre][word] += 1
+
+        voc_size = len(self.vocabulary)
+        for genre in self.conditionals:
+            total_words_genre = sum(self.conditionals[genre]) + self.alpha
+            for word in self.conditionals[genre]:
+                self.conditionals[genre][word] = self.conditionals[genre][word] / (total_words_genre + voc_size * self.alpha)
 
 
 
     def train(self, x_train, y_train):
-        self.calc_a_priori()
-        self.
+        self.calc_a_priori(y_train)
+        self.calc_cond_voc(x_train, y_train)
+
+    def predict(self, x_test):
+        y_test = []
+        for text in x_test:
+            vocabulary = {}
+            for word in text:
+                if word not in vocabulary:
+                    vocabulary[word] = 1
+                else:
+                    vocabulary[word] += 1
+            genre_prob = {}
+            for genre in self.conditionals:
+                prob = self.a_priori[genre]
+                for word in vocabulary:
+                    if word in self.vocabulary:
+                        prob += vocabulary[word] * np.log(self.conditionals[genre][word])
+                genre_prob[genre] = prob
+            maxx = -1
+            final_genre = ""
+            for genre in genre_prob:
+                if genre_prob > maxx:
+                    maxx = genre_prob, final_genre = genre
+            y_test.append(final_genre)
+        return y_test
+
+    def evaluate(self, x_test, y_test):
+        y_result = self.predict(x_test)
+        genres_accuracy = {}
+        total = len(y_test)
+        acc = 0
+        for result, i in zip(y_result, (0, len(y_test))):
+            correct = y_test[i]
+            if correct not in genres_accuracy:
+                genres_accuracy[correct] = {}
+            if result not in genres_accuracy[correct]:
+                genres_accuracy[correct][result] = 1
+            else:
+                genres_accuracy[correct][result] += 1
+            if result is not correct:
+                acc += 1
+        print("Model Accuracy: ", acc / total * 100, "%")
+
+
 
 
 
